83d7d08b18da0ea2b8aac14da991a77a5db9aca6
choi-day <choisschoco@gmail.com>
2025-12-12 12:18:12 +0900

    이미지 url 토큰삭제

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,4 +1,4 @@
-const prefix = 'https://firebasestorage.googleapis.com/v0/b/react-native-simple-chat-469fb.firebasestorage.app/o/logo.png?alt=media&token=a9a492de-ccdf-485c-897f-291776c8b9d6';
+const prefix = 'https://firebasestorage.googleapis.com/v0/b/react-native-simple-chat-469fb.firebasestorage.app/o';
 
 export const images = {
     logo: `${prefix}/logo.png?alt=media`,
