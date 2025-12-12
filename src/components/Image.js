e000face793eed88ef6b7d4ef1079843e4b6b6ea
choi-day <choisschoco@gmail.com>
2025-12-12 19:50:33 +0900

    이미지 삽입 버튼 컴포넌트 구현

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,35 +1,77 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import PropTypes from 'prop-types';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const Container = styled.View`
   align-self: center;
   margin-bottom: 30px;
+  position: relative;
 `;
 
 const StyledImage = styled.Image`
   background-color: ${({ theme }) => theme.imageBackground};
   width: 100px;
   height: 100px;
-  border-radius: ${({ rounded })=> (rounded? 50 : 0)}px;
+  border-radius: ${({ rounded }) => (rounded ? 50 : 0)}px;
 `;
 
-const Image = ({ uri, imageStyle, rounded }) => {
+const ButtonContainer = styled.TouchableOpacity`
+  background-color: ${({ theme }) => theme.imageButtonBackground};
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonIcon = styled(MaterialIcons).attrs({
+  name: 'photo-camera',
+  size: 22,
+})`
+  color: ${({ theme }) => theme.imageButtonIcon};
+`;
+
+const PhotoButton = ({ onPress }) => {
+  return (
+    <ButtonContainer onPress={onPress}>
+      <ButtonIcon />
+    </ButtonContainer>
+  );
+};
+
+PhotoButton.propTypes = {
+  onPress: PropTypes.func,
+};
+
+PhotoButton.defaultProps = {
+  onPress: () => {},
+};
+
+const Image = ({ url, imageStyle, rounded, showButton, onPress }) => {
   return (
     <Container>
-      <StyledImage source={{ uri: uri }} style={imageStyle} rounded = {rounded} />
+      <StyledImage source={{ uri: url }} style={imageStyle} rounded={rounded} />
+      {showButton && <PhotoButton onPress={onPress} />}
     </Container>
   );
 };
 
 Image.defaultProps = {
   rounded: false,
+  showButton: false,
+  onPress: () => {},
 };
 
 Image.propTypes = {
-  uri: PropTypes.string,
+  url: PropTypes.string,
   imageStyle: PropTypes.object,
   rounded: PropTypes.bool,
+  showButton: PropTypes.bool,
+  onPress: PropTypes.func,
 };
 
 export default Image;
