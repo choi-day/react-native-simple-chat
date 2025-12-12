f4fb4a3720db81746766985b3ef8c15d5e800d4c
choi-day <choisschoco@gmail.com>
2025-12-12 19:29:06 +0900

    회원가입 화면 이미지 원형 구현

--- src/components/Image.js
+++ src/components/Image.js
@@ -11,19 +11,25 @@ const StyledImage = styled.Image`
   background-color: ${({ theme }) => theme.imageBackground};
   width: 100px;
   height: 100px;
+  border-radius: ${({ rounded })=> (rounded? 50 : 0)}px;
 `;
 
-const Image = ({ uri, imageStyle }) => {
+const Image = ({ uri, imageStyle, rounded }) => {
   return (
     <Container>
-      <StyledImage source={{ uri: uri }} style={imageStyle} />
+      <StyledImage source={{ uri: uri }} style={imageStyle} rounded = {rounded} />
     </Container>
   );
+};
+
+Image.defaultProps = {
+  rounded: false,
 };
 
 Image.propTypes = {
   uri: PropTypes.string,
   imageStyle: PropTypes.object,
+  rounded: PropTypes.bool,
 };
 
 export default Image;
