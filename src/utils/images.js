bd7c22fc257c83b02a3918440bc73c1eb99b4cb8
choi-day <choisschoco@gmail.com>
2025-12-12 12:17:19 +0900

    파이어베이스에서 로고 이미지 불러오기

---
+++ src/utils/images.js
@@ -0,0 +1,5 @@
+const prefix = 'https://firebasestorage.googleapis.com/v0/b/react-native-simple-chat-469fb.firebasestorage.app/o/logo.png?alt=media&token=a9a492de-ccdf-485c-897f-291776c8b9d6';
+
+export const images = {
+    logo: `${prefix}/logo.png?alt=media`,
+};
