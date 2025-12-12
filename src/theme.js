46b07d99c1f520da5d98f84818153eabc3c326e7
choi-day <choisschoco@gmail.com>
2025-12-12 19:50:10 +0900

    이미지 삽입 버튼 색상 정의

--- src/theme.js
+++ src/theme.js
@@ -14,7 +14,10 @@ export const theme = {
   text: colors.black,
   errorText: colors.red,
 
-  ImageBackground: colors.grey_0,
+  imageBackground: colors.grey_0,
+  imageButtonBackground: colors.grey_1,
+  imageButtonIcon: colors.white,
+
   label: colors.grey_1,
   inputPlaceholder: colors.grey_1,
   inputBorder: colors.grey_1,
